d10321ad2236c379763333103978d3bf3bfd1010
Friox <ljykdltmdgns624@gmail.com>
2023-05-20 23:44:45 +0900

    [Feat] add APIUtils Class(#7)

---
+++ lib/utils/net/api_utils.dart
@@ -0,0 +1,26 @@
+import 'package:http/http.dart';
+import 'package:deu_project/models/api/user_auth.dart';
+import 'package:deu_project/models/api/user_auth_refresh.dart';
+import 'package:deu_project/models/api/user_create.dart';
+import 'package:deu_project/models/api/user_read.dart';
+import 'package:deu_project/models/api/user_update.dart';
+
+class APIUtils {
+
+  // 맡은 부분 아래부터 함수를 만들어 작성해주세요!
+  // COMMON (이부분은 건들지마세요)
+
+
+  // CREATE
+
+
+  // READ
+
+
+  // UPDATE
+
+
+  // DELETE
+
+
+}
