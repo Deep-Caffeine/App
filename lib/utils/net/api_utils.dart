e1470da4fa1797e408bc70fffe5bf07d441528a4
sunhaklee <lsh6076@naver.com>
2023-05-21 21:09:26 +0900

    [Feat] add API - Delete (#11)

--- lib/utils/net/api_utils.dart
+++ lib/utils/net/api_utils.dart
@@ -1,26 +1,25 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:deu_project/models/api/user_auth.dart';
 import 'package:deu_project/models/api/user_auth_refresh.dart';
 import 'package:deu_project/models/api/user_create.dart';
 import 'package:deu_project/models/api/user_read.dart';
 import 'package:deu_project/models/api/user_update.dart';
+import 'dart:convert';
 
 class APIUtils {
-
   // 맡은 부분 아래부터 함수를 만들어 작성해주세요!
   // COMMON (이부분은 건들지마세요)
 
-
   // CREATE
-
 
   // READ
 
-
   // UPDATE
 
-
   // DELETE
-
-
+  static const String url = "<https://www.ideaconnect.online/user>";
+  static Future<int> deleteUser() async {
+    final response = await http.delete(Uri.parse(url));
+    return response.statusCode;
+  }
 }
