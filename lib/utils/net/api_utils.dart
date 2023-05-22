f9ba9043059e8e6a92c5e392781b976fb447a76c
WooLegend <71242138+Woolegend@users.noreply.github.com>
2023-05-22 15:05:03 +0100

    [#13] CRUD API - Update (#10)

--- lib/utils/net/api_utils.dart
+++ lib/utils/net/api_utils.dart
@@ -6,25 +6,27 @@ import 'package:deu_project/models/api/user_auth_refresh.dart';
 import 'package:deu_project/models/api/user_create.dart';
 import 'package:deu_project/models/api/user_read.dart';
 import 'package:deu_project/models/api/user_update.dart';
+import 'dart:convert';
 
 String baseUrl = dotenv.env['API_BASE_HTTP']!;
 
 class APIUtils {
-
   // 맡은 부분 아래부터 함수를 만들어 작성해주세요!
   // COMMON (이부분은 건들지마세요)
 
-
   // CREATE
-
 
   // READ
 
-
   // UPDATE
-
+  static Future<UserUpdateResponse?> getUpdateInfo() async {
+    final response = await http.put(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return UserUpdateResponse.fromJson(jsonDecode(response.body));
+    } else {
+      return null;
+    }
+  }
 
   // DELETE
-
-
 }
